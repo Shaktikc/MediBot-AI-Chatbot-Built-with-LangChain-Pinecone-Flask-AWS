39a525518edcf2d195da2d224e0472db89dce78e
Shaktikc <87841609+Shaktikc@users.noreply.github.com>
2026-01-07 11:15:49 +1100

    Rename package

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="medical_chatbot",
+    name="MediBot-AI",
     version="0.1.0",
-    author="Boktiar Ahmed Bappy",
-    author_email="entbappy73@gmail.com",
+    author=Shakti KC",
     packages=find_packages(),
     install_requires=[]
 )
