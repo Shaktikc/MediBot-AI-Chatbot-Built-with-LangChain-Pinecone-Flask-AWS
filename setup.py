3c63fcdd6d2eb948dc74ae9b68383c5a9ef3520d
Shaktikc <87841609+Shaktikc@users.noreply.github.com>
2026-01-07 11:36:32 +1100

    Rename

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="MediBot-AI",
+    name="MediBot-AI-Chatbot-Built-with-LangChain-Pinecone-Flask-AWS",
     version="0.1.0",
     author=Shakti KC",
     packages=find_packages(),
